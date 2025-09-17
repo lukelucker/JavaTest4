2fc7f81caab639632e9d8bab67649f141f7b49ce
lukelucker <officiallucker@gmail.com>
2025-09-17 21:46:21 +0200

    Added JUnit in pom.xml and add() method to StringContainer, and Node logic.

---
+++ src/main/java/pl/kurs/task1/app/MainApp.java
@@ -0,0 +1,13 @@
+package pl.kurs.task1.app;
+
+import pl.kurs.task1.models.StringContainer;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        StringContainer st = new StringContainer("\\d{2}[-]\\d{3}");
+
+
+    }
+}
